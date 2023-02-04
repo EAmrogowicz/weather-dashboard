bdd14a10d3c9601945d481ec0659e72e6cd68034
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-02-04 16:23:11 +0000

    Weather and forecast function.

--- script.js
+++ script.js
@@ -1,9 +1,113 @@
+let pastSearches = [];
+const currentDay = ` (${moment().format("DD/MM/YYYY")})`;
+
+const forecastDay1 = moment().add(1, "days").format("DD/MM/YYYY");
+
+function appendSearch(search) {
+  const pastSearchesNo = 2;
+
+  if (pastSearches.length === pastSearchesNo) {
+    pastSearches.pop();
+  }
+
+  pastSearches.unshift(search);
+
+  $("#history").empty();
+
+  for (let i = 0; i < pastSearches.length; i++) {
+    let lastSearch = $("<button>").text(pastSearches[i]);
+    lastSearch.attr("class", "pastSearchBtn");
+    $("#history").append(lastSearch);
+  }
+}
+
+let citySearch = "London";
+
+function cityWeather(city) {
+  const APIKey = "ac05d0f083a97fd76b0305bca7bb2db8";
+
+  const queryURL =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    APIKey +
+    "&units=metric";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+
+    const weather = {
+      city: response.city.name,
+      icon: response.list[0].weather[0].icon,
+      temp: response.list[0].main.temp,
+      wind: response.list[0].wind.speed,
+      humidity: response.list[0].main.humidity,
+    };
+
+    const weatherOverview = $("<img>").attr(
+      "src",
+      "http://openweathermap.org/img/wn/" + weather.icon + "@2x.png"
+    );
+
+    const cityLabel = $("<h2>").text(weather.city + currentDay);
+    cityLabel.append(weatherOverview);
+    const tempP = $("<p>").text(`Temp: ${weather.temp} °C`);
+    const windP = $("<p>").text(`Wind: ${weather.wind} KPH`);
+    const humidityP = $("<p>").text(`Humidity: ${weather.humidity} %`);
+    $("#today").append(cityLabel, tempP, windP, humidityP);
+  });
+}
+
+function cityForecast(city) {
+  const APIKey = "ac05d0f083a97fd76b0305bca7bb2db8";
+
+  const queryURL =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    APIKey +
+    "&units=metric";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    for (let i = 1; i < 6; i++) {
+      const forecast = {
+        icon: response.list[i].weather[0].icon,
+        temp: response.list[i].main.temp,
+        wind: response.list[i].wind.speed,
+        humidity: response.list[i].main.humidity,
+      };
+
+      const forecastDiv = $("<div>").addClass("forecast-div");
+      const forecastDate = $("<h3>").text(forecastDay1);
+
+      const tempP = $("<p>").text(`Temp: ${forecast.temp} °C`);
+      const windP = $("<p>").text(`Wind: ${forecast.wind} KPH`);
+      const humidityP = $("<p>").text(`Humidity: ${forecast.humidity} %`);
+
+      $(".forecast-container").append(forecastDiv);
+      forecastDiv.append(forecastDate, tempP, windP, humidityP);
+    }
+
+    const forecastLabel = $("<h3>").text("5-Day Forecast:");
+    $("#forecast").prepend(forecastLabel);
+  });
+}
+
+cityWeather(citySearch);
+cityForecast(citySearch);
+
 $("#searchBtn").on("click", function (event) {
   event.preventDefault();
 
   const APIKey = "ac05d0f083a97fd76b0305bca7bb2db8";
 
-  // retrieve search input text
+  // retrieve user search input text
   const searchInput = $("#searchInput").val();
 
   const queryURL =
@@ -21,4 +125,9 @@ $("#searchBtn").on("click", function (event) {
     //
     //
   });
+
+  $("#searchInput").val("");
+
+  appendSearch(searchInput);
+  printSearch(searchInput);
 });
